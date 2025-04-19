f49f6bc99fde46dda665621bce2bc389b51905e3
Abhishek Shrestha <abhishek_24152353@sunway.edu.np>
2025-04-20 00:24:50 +0545

    fix: reorder imports for better readability

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -3,25 +3,25 @@ import { fetchByKey } from "@/utils/fetch";
 import { Metadata } from "next";
 import Image from "next/image";
 
-export async function generateMetadata(): Promise<Metadata> {
-    const data: any = await fetchByKey("overviewSeo");
+// export async function generateMetadata(): Promise<Metadata> {
+//     const data: any = await fetchByKey("overviewSeo");
 
-    const parsedData = JSON.parse(data?.value || "{}");
+//     const parsedData = JSON.parse(data?.value || "{}");
 
-    return {
-        title: {
-            default: parsedData?.title || "checks",
-            template: "%s | Prabhu Steels",
-        },
-        description: parsedData?.description || "Default description",
-        keywords: parsedData?.keywords || "Default keywords",
-        icons: {
-            icon: "/logo.ico",
-            apple: "/logo.ico",
-            shortcut: "/logo.ico",
-        },
-    };
-}
+//     return {
+//         title: {
+//             default: parsedData?.title || "checks",
+//             template: "%s | Prabhu Steels",
+//         },
+//         description: parsedData?.description || "Default description",
+//         keywords: parsedData?.keywords || "Default keywords",
+//         icons: {
+//             icon: "/logo.ico",
+//             apple: "/logo.ico",
+//             shortcut: "/logo.ico",
+//         },
+//     };
+// }
 
 const page = () => {
     return (
