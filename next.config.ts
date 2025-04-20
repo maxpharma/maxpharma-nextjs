737c6ac04318ec31ed2e6d59ed977e1889018c52
Inflancer Technology <154499870+inflancer@users.noreply.github.com>
2025-04-20 17:20:58 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "maxpharma.inflancer.com",
+                hostname: "maxpharma.com.np",
             },
         ],
     },
