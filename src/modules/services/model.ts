8cc0b38be6ad50c4d9d208544966328e6f829bbf
KISMAT1234 <kismatbohora123@gmail.com>
2025-04-20 11:58:23 +0545

    category joined in service table

--- src/modules/services/model.ts
+++ src/modules/services/model.ts
@@ -1,4 +1,5 @@
 import db from "../../config/db";
+import GeneralSetting from "../generalSettings/model";
 import { serviceAttributes } from "./attributes";
 const Service = db.define("services", serviceAttributes, {
   tableName: "services",
@@ -6,5 +7,22 @@ const Service = db.define("services", serviceAttributes, {
   createdAt: "createdAt",
   updatedAt: "updatedAt",
 });
+
+Service.belongsTo(GeneralSetting, {
+  foreignKey:"categoryId",
+  as:"categoryData"
+})
+
+Service.addScope("withCategory", () => {
+  const scope = {
+    model:GeneralSetting,
+    as:"categoryData",
+    where:{}
+  }
+
+  return {
+    include: [scope]
+  }
+})
 
 export default Service;
