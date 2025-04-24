44744a04af6798d38117aa652ff0b96a3cb87f7b
Inflancer Technology <154499870+inflancer@users.noreply.github.com>
2025-04-24 10:31:45 +0545

    Update service.ts

--- src/modules/services/service.ts
+++ src/modules/services/service.ts
@@ -62,7 +62,7 @@ const update = async (input: any, id: number) => {
     const data: any = await find({
       id:id
     });
-    if(Constant.imageValidationExtensions.includes(input?.files?.extension)){
+    if(Constant.imageValidationExtensions.includes(input?.files?.extension?.toLowerCase?.()){
       input.files = await uploadMultipleImage(input?.files, 'services', data?.files)
     }
     await data.update(input);
