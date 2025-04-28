2f5d860a6651c16d729d108b0e8dfe86e6504190
Abhishek Shrestha <abhishek_24152353@sunway.edu.np>
2025-04-28 12:48:13 +0545

    refactor: Improve code formatting and structure in Page component

--- src/app/(pages)/about/organization-hierarchy/page.tsx
+++ src/app/(pages)/about/organization-hierarchy/page.tsx
@@ -1,6 +1,5 @@
 import { getSeoMetadata } from "@/utils/seo";
 import OrganizationChartPage from "./OrganizationChartPage";
-import ScrollReveal from "@/components/animation/ScrollReveal";
 
 export async function generateMetadata() {
     return await getSeoMetadata("organizationHistorySeo");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,77 +19,79 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const Page = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <>
-      <div className="custom-container mt-4 flex flex-col gap-4 md:gap-16">
-        <ScrollReveal>
-          <Banner />
-        </ScrollReveal>
+    return (
+        <>
+            <div className='custom-container mt-4 flex flex-col gap-4 md:gap-16'>
+                <ScrollReveal>
+                    <Banner />
+                </ScrollReveal>
 
-        <ScrollReveal>
-          <AboutUs />
-        </ScrollReveal>
-        <ScrollReveal>
-          <AdvertisementBanner />
-        </ScrollReveal>
+                <ScrollReveal>
+                    <AboutUs />
+                </ScrollReveal>
+                <ScrollReveal>
+                    <AdvertisementBanner />
+                </ScrollReveal>
 
-        <ScrollReveal>
-          <section className="space-y-4">
-            <Products variant="scroll" />
-          </section>
-        </ScrollReveal>
+                <ScrollReveal>
+                    <section className='space-y-4'>
+                        <Products variant='scroll' />
+                    </section>
+                </ScrollReveal>
 
-        <ScrollReveal>
-          <section className="space-y-4  mt-8">
-            <Notices limit={3} />
-          </section>
-        </ScrollReveal>
+                <ScrollReveal>
+                    <section className='space-y-4  mt-8'>
+                        <Notices limit={3} />
+                    </section>
+                </ScrollReveal>
 
-        <ScrollReveal>
-          <section className="bg-light-primary rounded-2xl px-4 py-8">
-            <div className="flex max-sm:flex-col items-center justify-between">
-              <div className="flex flex-col gap-4 md:gap-8  max-w-2xl">
-                <div className="text-lg md:text-2xl lg:text-4xl font-semibold">
-                  Caring For Your Health, Every Step Of The Way
-                </div>
-                <p>
-                  From trusted medications to expert healthcare guidance—your
-                  wellness is our priority. Got questions? We’re here to help!
-                </p>
-                <Link href={"/contact"}>
-                  {" "}
-                  <Button>Let&apos;s Talk</Button>
-                </Link>
-              </div>
-              <div className="ml-auto">
-                <CustomImage
-                  src="/images/ad-banner.png"
-                  className="w-48 h-48 md:w-64 md:h-64 "
-                />
-              </div>
+                <ScrollReveal>
+                    <section className='bg-light-primary rounded-2xl px-4 py-8'>
+                        <div className='flex max-sm:flex-col items-center justify-between'>
+                            <div className='flex flex-col gap-4 md:gap-8  max-w-2xl'>
+                                <div className='text-lg md:text-2xl lg:text-4xl font-semibold'>
+                                    Caring For Your Health, Every Step Of The
+                                    Way
+                                </div>
+                                <p>
+                                    From trusted medications to expert
+                                    healthcare guidance—your wellness is our
+                                    priority. Got questions? We’re here to help!
+                                </p>
+                                <Link href={"/contact"}>
+                                    {" "}
+                                    <Button>Let&apos;s Talk</Button>
+                                </Link>
+                            </div>
+                            <div className='ml-auto'>
+                                <CustomImage
+                                    src='/images/ad-banner.png'
+                                    className='w-48 h-48 md:w-64 md:h-64 '
+                                />
+                            </div>
+                        </div>
+                    </section>
+                </ScrollReveal>
+                <ScrollReveal>
+                    <section className='space-y-4'>
+                        <Faqs type='Home' />
+                    </section>
+                </ScrollReveal>
+                <ScrollReveal>
+                    <section>
+                        <BottomBanners />
+                    </section>
+                </ScrollReveal>
+                <ScrollReveal>
+                    <section>
+                        <ContactUs />
+                    </section>
+                </ScrollReveal>
             </div>
-          </section>
-        </ScrollReveal>
-        <ScrollReveal>
-          <section className="space-y-4">
-            <Faqs type="Home" />
-          </section>
-        </ScrollReveal>
-        <ScrollReveal>
-          <section>
-            <BottomBanners />
-          </section>
-        </ScrollReveal>
-        <ScrollReveal>
-          <section>
-            <ContactUs />
-          </section>
-        </ScrollReveal>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Page;
